12e9adc381cd683a04ee575ad6e2fc2b83d99b40
Mazen ELNoby <mazenelnobyec@gmail.com>
2022-10-19 21:03:11 +0200

    added id setter and range creator functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { tablePrams , testTable } from "./modules/generator.js";
 
-let testingTable  = {rows:4, columns:5};
+let testingTable  = {rows:4, columns:50, startingID:52134};
 
 tablePrams(testingTable)
 testTable()
--- modules/generator.js
+++ modules/generator.js
@@ -1,15 +1,32 @@
 let tableParamters = {
     rows:null,
     columns:null,
+    startingID:null
 }
 
 export function tablePrams(params) {
     tableParamters={rows:params.rows,
-                    columns:params.columns}
+                    columns:params.columns,
+                    startingID:params.startingID}
 }
 
 export function testTable(){
     console.log(`${tableParamters.rows}   ${tableParamters.columns}`)
+    console.log(idSetter(tableParamters.columns,tableParamters.startingID))
 }
 
 
+function idSetter(lengthOfColumn,startingID){
+    const arrayOfLines=[]
+    for (let index = 0; index < lengthOfColumn; index++) {
+        arrayOfLines.push({"ID":startingID,
+                          "Length":rangeCreator(100,250)});
+        startingID += 1;
+        
+    }
+    return arrayOfLines;
+}
+
+function rangeCreator(min,max){
+    return Math.random() * (max-min + 1 ) + min 
+}
