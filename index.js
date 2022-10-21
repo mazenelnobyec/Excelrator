00a8fc5011dcfbf7086e2278cd187c46dc09f76b
Mazen ELNoby <mazenelnobyec@gmail.com>
2022-10-21 14:37:55 +0200

    renamed generator to table_generator

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { tablePrams , testTable } from "./modules/generator.js";
+import { tablePrams , testTable } from "./modules/table_generator.js";
 
 let testingTable  = {rows:4, columns:50, startingID:52134};
 
