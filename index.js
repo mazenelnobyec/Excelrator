280afa3e43702a1c1f5580db325d59ded7ecf248
Mazen ELNoby <mazenelnobyec@gmail.com>
2022-11-04 17:16:55 +0200

    removed useless libs

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import { ClothTable } from "./table_generator.js";
-
 let testingTable  = {rows:50, columns:['length','width','weight'], startingID:52134};
 
 const table = new ClothTable(testingTable.rows,testingTable.startingID,50,100,45)
 
 table.idSetter()
 table.printTable()
-
 
